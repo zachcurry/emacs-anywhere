29978f9280a8029d4dc44b94d5ea522bc1220c56
Sibi Prabakaran <sibi@psibi.in>
2018-03-04 23:25:48 +0530

    Port it for my xmonad setup

--- emacs_anywhere.el
+++ emacs_anywhere.el
@@ -1,14 +1,19 @@
+(require 'f)
+
 (defun ea-on-delete (frame)
   (clipboard-kill-ring-save
    (point-min)
    (point-max))
   (kill-buffer "*Emacs Anywhere*"))
 
+(defun ea-on-delete2 (frame)
+  (f-write (buffer-string) 'utf-8 "/home/sibi/.emacs_anywhere/clipboard"))
+
 (defun ea-hook ()
-  (add-hook 'delete-frame-functions 'ea-on-delete))
+  (add-hook 'delete-frame-functions 'ea-on-delete2))
 
 (defun ea-unhook ()
-  (remove-hook 'delete-frame-functions 'ea-on-delete))
+  (remove-hook 'delete-frame-functions 'ea-on-delete2))
 
 (defun emacs-anywhere ()
   (interactive)
