7e46454a1839af5734653d26bdbe591b46331d4e
Zach Curry <zach.getatme@gmail.com>
2018-03-19 19:07:50 -0600

    Allow user to control copy behavior via ea-popup-hook. Closes #49.

--- emacs_anywhere.el
+++ emacs_anywhere.el
@@ -2,6 +2,7 @@
   "Functions run after Emacs Anywhere creates a new frame and buffer.")
 
 (defvar ea-on t)
+(defvar ea-copy t)
 (defvar ea-paste t)
 
 (defconst ea--buffer-name "*Emacs Anywhere*")
@@ -26,18 +27,23 @@
 
 (defun ea--delete-frame-handler (_frame)
   (remove-hook 'delete-frame-functions 'ea--delete-frame-handler)
-  (when ea-on
+  (when (and ea-on ea-copy)
     (cond
      (ea--osx (ea--osx-copy-to-clip))
-     (ea--gnu-linux (ea--gnu-linux-copy-to-clip)))
-    (kill-buffer ea--buffer-name))
+     (ea--gnu-linux (ea--gnu-linux-copy-to-clip))))
+  (when ea-on (kill-buffer ea--buffer-name))
   (shell-command
-   (format "echo export EA_ABORT=%s\";\" export EA_SHOULD_PASTE=%s > /tmp/eaenv"
+   (format (concat "echo export EA_ABORT=%s\";\""
+                   "export EA_SHOULD_COPY=%s\";\""
+                   "export EA_SHOULD_PASTE=%s"
+                   " > /tmp/eaenv")
            (if ea-on "false" "true")
+           (if ea-copy "true" "false")
            (if ea-paste "true" "false"))))
 
 (defun ea--init ()
   (setq ea-on t) ; begin each session with EA enabled
+  (setq ea-copy t) ; begin each session with copy enabled
   (setq ea-paste t) ; begin each session with paste enabled
   (add-hook 'delete-frame-functions 'ea--delete-frame-handler)
   (switch-to-buffer ea--buffer-name)
